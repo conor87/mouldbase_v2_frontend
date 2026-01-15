14dc959aa9e842c79830178a0a797d80612e87b5
karol <karoldabko@gmail.com>
2026-01-15 09:27:16 +0300

    server

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,4 @@
-//export const DEFAULT_API_BASE = "http://10.10.77.75:8000";
-export const DEFAULT_API_BASE = "http://localhost:8000";
-export const API_BASE = import.meta?.env?.VITE_API_BASE ?? DEFAULT_API_BASE;
+export const DEFAULT_API_BASE = "http://10.10.77.75:8000";
+//export const DEFAULT_API_BASE = "http://localhost:8000";
+//export const API_BASE = import.meta?.env?.VITE_API_BASE ?? DEFAULT_API_BASE;
+export const API_BASE = DEFAULT_API_BASE;
